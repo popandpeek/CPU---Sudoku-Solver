7f9993e72a5e32cdd265b85eafa06a5e95471686
Benjamin D. Pittman <benpitts@uw.edu>
2019-03-12 08:57:14 -0700

    Updated annotate_potential_function to track can't have values as true, can have as false

--- Sudoku Solver/board.cpp
+++ Sudoku Solver/board.cpp
@@ -58,18 +58,8 @@ void Board::set_cell(int _row, int _col, int _val) {
 void Board::annotate_potential_entries() {
 	// std::cout << empty_cells << std::endl;
 	for (int row = 0; row < SUB_BOARD_SIZE; row++) {
-		// find non-filled values in the row and add to set
+		// set to hold non-filled values in the row
 		std::set<int> row_vals;
-		for (int n = row * SUB_BOARD_SIZE; n < SUB_BOARD_SIZE + (row * SUB_BOARD_SIZE); n++) {
-			if (board[n][0] == true) {
-				for (int k = 1; k < SUB_BOARD_SIZE + 1; k++) {
-					if (board[n][k] == true) {
-						row_vals.insert(k);
-						break;
-					}
-				}
-			}
-		}
 
 		// std::cout << row_vals.size() << std::endl;
 		// remove values from set that correspond to filled cells in the row
@@ -77,14 +67,14 @@ void Board::annotate_potential_entries() {
 			if (board[i][0] == true) {
 				for (int j = 1; j < SUB_BOARD_SIZE; j++) {
 					if (board[i][j] == true) {
-						row_vals.erase(j);
+						row_vals.insert(j);
 					}
 				}
 			}
 		}
 
 		// std::cout << "Got to 76" << std::endl;
-		// Fill potential entries in rows
+		// Fill cells with true where indeces correspond to values it cannot have 
 		if (!row_vals.empty()) {
 			for (int i = row * SUB_BOARD_SIZE; i < (row * SUB_BOARD_SIZE) + SUB_BOARD_SIZE; i++) {
 				if (board[i][0] == false) {
@@ -92,17 +82,17 @@ void Board::annotate_potential_entries() {
 						board[i][*it] = true;
 					}
 
-					// check for single potential value and set if true
-					int count = 0;
-					for (int j = 1; j < SUB_BOARD_SIZE; j++) {
-						if (board[i][j] == true) {
-							++count;
-						}
-					}
-					if (count == 1) {
-						board[i][0] = true;
-						--empty_cells;
-					}
+					//// check for single potential value and set if true
+					//int count = 0;
+					//for (int j = 1; j < SUB_BOARD_SIZE; j++) {
+					//	if (board[i][j] == true) {
+					//		++count;
+					//	}
+					//}
+					//if (count == 1) {
+					//	board[i][0] = true;
+					//	--empty_cells;
+					//}
 				}
 			}
 		}
@@ -112,6 +102,7 @@ void Board::annotate_potential_entries() {
 	// scan col for filled in values and store in temp set
 	for (int col = 0; col < SUB_BOARD_SIZE; col++) {
 		std::set<int> col_vals;
+		
 		for (int i = col; i < BOARD_SIZE - SUB_BOARD_SIZE + col + 1; i += SUB_BOARD_SIZE) {
 			// std::cout << "Got to 106" << std::endl;
 			if (board[i][0] == true) {
@@ -125,27 +116,27 @@ void Board::annotate_potential_entries() {
 		}
 		// std::cout << col_vals.size() << std::endl;
 		// std::cout << "Got to 115" << std::endl;
-		// Reduce potential entries for column intersections
+		// Fill cells with true where indeces correspond to values it cannot have 
 		if (!col_vals.empty()) {
 			for (int i = col; i < BOARD_SIZE - SUB_BOARD_SIZE + col; i += SUB_BOARD_SIZE) {
 				if (board[i][0] == false) {
 					for (auto it = col_vals.begin(); it != col_vals.end(); ++it) {
-						if (board[i][*it] == true) {
-							board[i][*it] = false;
+						if (board[i][*it] == false) {
+							board[i][*it] = true;
 						}
 					}
 
-					// check for single potential value and set if true
-					int count = 0;
-					for (int j = 1; j < SUB_BOARD_SIZE; j++) {
-						if (board[i][j] == true) {
-							++count;
-						}
-					}
-					if (count == 1) {
-						board[i][0] = true;
-						--empty_cells;
-					}
+					//// check for single potential value and set if true
+					//int count = 0;
+					//for (int j = 1; j < SUB_BOARD_SIZE; j++) {
+					//	if (board[i][j] == true) {
+					//		++count;
+					//	}
+					//}
+					//if (count == 1) {
+					//	board[i][0] = true;
+					//	--empty_cells;
+					//}
 				}
 			}
 		}
@@ -173,24 +164,24 @@ void Board::annotate_potential_entries() {
 			for (int row = 0; row < SUB_BOARD_DIM; row++) {
 				for (int col = 0; col < SUB_BOARD_DIM; col++) {
 					int loc = (grid_x + row) * (grid_y + col);
-					if (!board[loc][0]) {
+					if (board[loc][0] == false) {
 						for (auto it = grid_vals.begin(); it != grid_vals.end(); ++it) {
-							if (board[loc][*it] == true) {
-								board[loc][*it] = false;
+							if (board[loc][*it] == false) {
+								board[loc][*it] = true;
 							}
 						}
 
-						// check for single potential value and set if true
-						int count = 0;
-						for (int i = 1; i < SUB_BOARD_SIZE; i++) {
-							if (board[loc][i] == true) {
-								++count;
-							}
-						}
-						if (count == 1) {
-							board[loc][0] = true;
-							--empty_cells;
-						}
+						//// check for single potential value and set if true
+						//int count = 0;
+						//for (int i = 1; i < SUB_BOARD_SIZE; i++) {
+						//	if (board[loc][i] == true) {
+						//		++count;
+						//	}
+						//}
+						//if (count == 1) {
+						//	board[loc][0] = true;
+						//	--empty_cells;
+						//}
 					}
 				}
 			}
